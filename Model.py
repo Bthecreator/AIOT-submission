23f45e150e616378d6e50e9085fe7dc2a03434ad
Bhavik Malik <77535684+Bthecreator@users.noreply.github.com>
2023-08-10 22:09:02 +0530

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,66 @@
+import cv2
+import time
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+import pyfirmata
+
+board = pyfirmata.Arduino('COM3')
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+maskClassifier = load_model('/home/bhavik/Desktop/folders/Mask AI/Model.h5')
+
+while True:
+   
+    _, frame = cap.read()
+
+    if not _:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        )
+    
+    for (x, y, h, w) in faces:
+        if withmask>withoutmask:
+            board.digital[2].write(1)
+        else:
+            board.digital[2].write(0)
+
+        faceROI = frame[y : y + h, x : x + w, :]
+
+        faceROI = cv2.resize(faceROI, (160, 160))
+        faceROI = img_to_array(faceROI)
+        faceROI = faceROI.reshape(1, 160, 160, 3)
+
+        prediction = maskClassifier(faceROI)
+        (withoutmask, withmask) = prediction[0].numpy()
+
+
+        (label, color, prob) = ('Mask', (0, 255, 0), withmask*100.0) if withmask > withoutmask else ('No mask', (0, 0, 255), withoutmask*100.0)
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+        cv2.rectangle(frame, (x + 15, y + 2), (x + w - 15, y + 20), (0, 0, 0), -1) #lower
+        cv2.rectangle(frame, (x + 15, y - 2), (x + w - 15, y - 20), (0, 0, 0), -1) #upper
+
+        cv2.putText(frame, str(prob)+' %', (x + 20, y - 7), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.putText(frame, label, (x + 20, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+
+        
+    cv2.imshow('Video', frame)
+    
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ README.txt
@@ -0,0 +1,23 @@
+Run Model.py to run the Classification model. 
+
+While the Python script is running, press q to kill it and close the model.
+
+If the python script does not work install the libraries mentioned in requirements.txt
+
+You can do so by opening a command promt with the location of this folder and entering the command ' pip install -r requirements.txt ' 
+
+The Model.ipynb file is a Jupyter notebook that was used to construct the model.
+
+The Model.h5 file is the AI model which was constructed by the Model.ipynb file and saved.
+
+The 'Face-Mask-Detection' folder contains the databse which was used to train the Model.
+
+The model was built with the help of the follwing libraries:
+
+1. Tensorflow
+2. Keras
+3. Imutils
+4. Numpy
+5. Opencv
+6. Matplotlib
+7. Scipy
